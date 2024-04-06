965c6d9de3002bd4e71d5e709b0b28b5b58dd286
lamaInstagrama <buanov08@gmail.com>
2024-04-06 11:40:45 +0300

    Add new validators for form 'title'

--- women/forms.py
+++ women/forms.py
@@ -3,7 +3,17 @@ from women.models import Category, Husband, TagPost
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    title = forms.CharField(max_length=255,
+                            min_length=5,
+                            error_messages={
+                                'max_length': 'Имя актрисы не такое длинное',
+                                'min_length': 'Имя актрисы не такое короткое',
+                                'required': 'Нужно ввести имя!'
+                            },
+                            label='Заголовок',
+                            widget=forms.TextInput(
+                                attrs={'class': 'form-input'}
+                            ))
     slug = forms.SlugField(max_length=255, label='Слаг')
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')
     is_published = forms.BooleanField(required=False, label='Опубликовать', initial=True)
